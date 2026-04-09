f8dece3587ec2f53bf5a1c06a8501b7e2091395e
Danub2002 <andrade20danilo@gmail.com>
2026-04-09 20:59:30 -0300

    fix: add dummy GEMINI_API_KEY for testing purpose

--- app/config.py
+++ app/config.py
@@ -10,11 +10,11 @@ class Settings(BaseSettings):
     SYNC_DATABASE_URL: str = "postgresql+psycopg2://whats_bot:whats_bot@localhost:5432/whats_bot"
     REDIS_URL: str = "redis://localhost:6379/0"
 
-    META_VERIFY_TOKEN: str = ""
+    META_VERIFY_TOKEN: str = "vibecode"
     META_API_TOKEN: str = ""
     META_PHONE_NUMBER_ID: str = ""
 
-    GEMINI_API_KEY: str = ""
+    GEMINI_API_KEY: str = "N Tem ainda :("
 
 
 settings = Settings()
