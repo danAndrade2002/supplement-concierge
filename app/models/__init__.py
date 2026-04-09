9d570f729165e7dad095336b63874758c6c297f3
Danub2002 <andrade20danilo@gmail.com>
2026-04-09 20:53:44 -0300

    fix: remove undefined models references

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,14 +1,6 @@
-from app.models.user import UserCreate, UserResponse
-from app.models.chat_message import ChatMessageCreate, ChatMessageResponse
-from app.models.reminder import ReminderCreate, ReminderResponse
+
 from app.models.incoming_message import IncomingMessage
 
 __all__ = [
-    "UserCreate",
-    "UserResponse",
-    "ChatMessageCreate",
-    "ChatMessageResponse",
-    "ReminderCreate",
-    "ReminderResponse",
     "IncomingMessage",
 ]
